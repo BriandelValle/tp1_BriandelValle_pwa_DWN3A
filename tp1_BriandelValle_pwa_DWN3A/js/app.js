c5cec419f2d3bc29d2893ae22353d74e53025201
BriandelValle <brian.delvalle@edu.ar>
2021-10-01 10:06:06 -0300

    Tp1 pwa
    Clima

---
+++ tp1_BriandelValle_pwa_DWN3A/js/app.js
@@ -0,0 +1,135 @@
+
+/* // API KEY // */
+
+const weatherApi = {
+    key: "e2a9acbd1027840dfa29285f0a381a2a",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather", 
+    idioma: "es"
+}
+
+const searchInputBox = document.getElementById('input-box');
+
+const valorStorage = JSON.parse(localStorage.getItem('RespuestaLocal'));
+
+if (valorStorage != null){
+    showWeatherReport(valorStorage);
+}
+
+
+searchInputBox.addEventListener('keypress', (event) => {
+    
+    if(event.keyCode == 13) {
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weather-body').style.display = "block";
+    }
+});
+
+
+
+/* // Get IM METEOROLOGICO //  */
+
+function getWeatherReport(city) {
+    /* fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`) */
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&lang=${weatherApi.idioma}&units=metric`)
+    .then(function (weather){
+        return weather.json();
+    }).then(function (weatherJSON){
+        showWeatherReport(weatherJSON);
+        saveLocalStorage(weatherJSON);
+    }).catch(function (error){
+        console.log('se trulo!',error);
+        
+    });
+}
+
+function saveLocalStorage (city){
+    localStorage.setItem('RespuestaLocal', JSON.stringify(city));
+}
+
+/* // IM METEOROLOGICO //  */
+
+function showWeatherReport(weather){
+    console.log(weather);
+
+    let icon = weather.weather[0].icon;
+
+    document.querySelector(".icon").src = 
+    "http://openweathermap.org/img/wn/" + icon + "@4x.png"; // -> iconoooo 
+
+    document.querySelector('.icon').style.display = "block";
+    
+    let city = document.getElementById('city');
+    city.innerText = `Ciudad: ${weather.name}, ${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `Temperatura: ${Math.round(weather.main.temp)}&deg;C`;
+
+    let feelsLike = document.getElementById('feelsLike');
+    feelsLike.innerHTML = `Sensación Térmica: ${Math.round(weather.main.feels_like)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+    
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `Clima Actual: ${weather.weather[0].description}`;
+
+    let hum = document.getElementById('hum');
+    hum.innerText = `Humedad: ${weather.main.humidity}%`;
+
+    let press = document.getElementById('press');
+    press.innerText = `Presion: ${weather.main.pressure} hPa`;
+
+    let wind = document.getElementById('wind');
+    wind.innerText = `Viento: ${weather.wind.speed} km/h`;
+    
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+
+    date.innerText = dateManage(todayDate);
+
+}
+
+/* // CONDICIONAL img // */
+    
+/*     if(weatherType.textContent == 'Clear') {
+        document.querySelector(".icon").src = 'http://openweathermap.org/img/wn/01d@2x.png';
+        
+    } else if(weatherType.textContent == 'Clouds') {
+
+        document.querySelector(".icon").src = 'http://openweathermap.org/img/wn/04d@2x.png';
+        
+    } else if(weatherType.textContent == 'Haze') {
+
+        document.querySelector(".icon").src = 'http://openweathermap.org/img/wn/50d@2x.png';
+        
+    }     else if(weatherType.textContent == 'Rain') {
+        
+        document.querySelector(".icon").src ='http://openweathermap.org/img/wn/10d@2x.png';
+        
+    } else if(weatherType.textContent == 'Snow') {
+        
+        document.querySelector(".icon").src = 'http://openweathermap.org/img/wn/13d@2x.png';
+    
+    } else if(weatherType.textContent == 'Thunderstorm') {
+    
+        document.querySelector(".icon").src ='http://openweathermap.org/img/wn/11d@2x.png';
+        
+    } 
+} */
+
+// Date manage
+function dateManage(dateArg) {
+
+    let days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+    let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} de ${month} (${day}), ${year}`;
+}
---
+++ tp1_BriandelValle_pwa_DWN3A/js/mapp.js
@@ -0,0 +1,11 @@
+function initMap(){
+    var coord = {lat:-26.8198 ,lng: -65.2169};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
